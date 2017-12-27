4df185c8bb1664944c04552dfd67cc12de71a0a0
Peter Johnstone <peter.wdj@gmail.com>
2017-12-27 23:47:52 +0000

    Add second test route

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello, world!');
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {});
+
+app.get('/test', function(req, res) {
+  // res.send('test.html')
 });
 
 app.listen(3000, function() {
