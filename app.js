384bed96cfe4a99e92b508fa05a1a4e34f50d7c3
Peter Johnstone <peter.wdj@gmail.com>
2017-12-28 11:27:55 +0000

    Auto-created Node modules files from pug

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'))
+app.set('views', './public/views');
+app.set('view engine', 'pug');
 
-app.get('/', function(req, res) {});
+app.use(express.static('public'));
 
-app.get('/test', function(req, res) {});
+app.get('/', function(req, res) {
+  res.render('index', {title: 'Hey', message: 'Hello there' });
+});
+
+app.route('/test')
+  .get(function(req, res) {
+    res.send('test')
+  })
+  .post(function(req, res) {
+    res.send('Hello, world')
+  })
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
